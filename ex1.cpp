305e1d87460235176e52e53be0ba8ed1f409a3d4
alin_alok <alokmahtab88@gmail.com>
2022-01-27 17:33:06 +0600

    conditional example improved

---
+++ ex1.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"enter the value of a: ";
+    cin>>a;
+    cout<<" value of a: "<<a;
+    if(a==0){
+        cout<<"- zero";
+    }
+    else if(a==1){
+        cout<<"- one";
+    }
+    else if(a==2){
+        cout<<"- two";
+    }
+    else if(a==3){
+        cout<<"- three";
+    }
+    else if(a==4){
+        cout<<"- four";
+    }
+    else if(a==5){
+        cout<<"- five";
+    }
+    else if(a==6){
+        cout<<"- six";
+    }
+    else if(a==7){
+        cout<<"- seven";
+    }
+    else if(a==8){
+        cout<<"- eight";
+    }
+    else if(a==9){
+        cout<<"- nine";
+    }
+    else{
+        cout<<" Greater then 9";
+    }
+    return 0;
+}
