a8fe36bc527423f5b21c4d6870a37ddea26dd80e
alin_alok <alokmahtab88@gmail.com>
2022-02-10 08:22:26 +0600

    radius of a circle

---
+++ Data Structure/circle_area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath.h>
+
+int main(){
+    double radius,area, pi;
+    cin>>r;
+    pi=acos(-1).;
+    area = pi**r;
+    cout<<"This is the area "<<area<<endl;
+
+    return 0;
+}
