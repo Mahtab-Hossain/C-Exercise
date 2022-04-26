29aa3210d7a80b77f77081aadc5fca6a7c786871
alin_alok <alokmahtab88@gmail.com>
2022-04-26 16:59:25 +0600

    moded class exercise

--- Class Exercise/class.cpp
+++ Class Exercise/class.cpp
@@ -12,24 +12,26 @@ public:
 
 int main(){
 
-    student s1;
-    s1.name= "Tabassum";
-    s1.ID= "20-43094-1";
-    s1.semester="Spring";
-    s1.department= "CSE";
-    s1.email= "tabassum@aiub.edu";
-
-    cout << s1.name << endl;
-    cout << s1.email << endl;
-
-    student s2;
-    s2.name= "Digonto";
-    s2.ID="21-45695-3";
-    s2.semester="Spring";
-    s2.department= "CSE";
-    s2.email= "digonto@aiub.edu";
-
-    cout << s2.semester << endl;
-    cout << s2.ID << endl;
+    student s1,s2;
+    
+    s1.name;
+    s1.ID;
+    s1.semester;
+    s1.department;
+    s1.email;
+    
+    cout<<"Enter your Name,Id,semester,department and email"<<endl;
+    cin>>s1.name>>s1.ID>>s1.semester>>s1.department>>s1.email;
+    
+    s2.name;
+    s2.ID;
+    s2.semester;
+    s2.department;
+    s2.email;
+    
+    cin>>s2.name>>s2.ID>>s2.semester>>s2.department>>s2.email;
+    cout<< "Name\tID\tSemester\tDeapartment\tMail "<<endl;
+    cout << s1.name <<"\t"<<s1.ID<<"\t"<<s1.semester<<"\t"<< s1.department <<"\t"<<s1.email<< endl;
+    cout << s2.name <<"\t"<<s2.ID<<"\t"<<s2.semester<<"\t"<< s2.department <<"\t"<<s2.email<< endl;
     return 0;
 }
